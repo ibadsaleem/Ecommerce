a77afcda6ebb42f0eaf87e4e3b2591c14df57fdc
unknown <you@example.com>
2021-12-11 20:59:14 +0500

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,10 @@ function App({ Component, pageProps }) {
             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
             crossOrigin="anonymous"
           />
+          <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+          rel="stylesheet"
+        />
           <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
